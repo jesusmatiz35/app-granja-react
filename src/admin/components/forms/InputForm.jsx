c131835e156f8fb4ba5f99cfcf220cdaeace01bc
Jesus Matiz <jesusmatiz35@gmail.com>
2025-02-22 11:08:59 -0500

    se agrega atributos adicionales

--- src/admin/components/forms/InputForm.jsx
+++ src/admin/components/forms/InputForm.jsx
@@ -1,10 +1,15 @@
-export const InputForm = ({ className='col-md', type = 'text', label = 'Label', name = 'date', defaultValue = '', onInputChange }) => {
+export const InputForm = ({ className='col-md', type = 'text', label = 'Label', name = 'date', defaultValue = '', min=0, disabled=false, onInputChange }) => {
+  
+  const isNumber = type === 'number';
+
   return (
     <div className={className}>
       <label>{label}</label>
       <input
         type={type}
         name={name}
+        min={min}
+        disabled={disabled}
         defaultValue={defaultValue}
         className="form-control"
         onChange={onInputChange}
--- src/admin/components/forms/SelectInputForm.jsx
+++ src/admin/components/forms/SelectInputForm.jsx
@@ -1,11 +1,10 @@
-export const SelectInput = ({ label = 'LabelText', className = "col-md", defaultValue = '', options = [], onSelectChange }) => {
+export const SelectInput = ({ label = 'LabelText', name='shed', className = "col-md", defaultValue = '', options = [], onSelectChange }) => {
   return (
     <div className={className}>
       <label>{label}</label>
       <select
-        id="shed"
         onChange={onSelectChange}
-        name="shed"
+        name={name}
         className="form-select form-select-sm"
         style={{ height: "38px" }}
         defaultValue={defaultValue}>
