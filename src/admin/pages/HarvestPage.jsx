00ee56265a7a060e1ec7c712c0c212e4d7acb6bf
Jesus Matiz <jesusmatiz35@gmail.com>
2025-02-21 19:42:10 -0500

    se inicia con el componente de recolección de huevos

--- src/admin/pages/HarvestPage.jsx
+++ src/admin/pages/HarvestPage.jsx
@@ -1,10 +1,80 @@
-import { Breadcrumb } from "../../ui/components/Breadcrumb"
+import { Breadcrumb } from "../../ui/components/Breadcrumb";
+import { Table } from "../../ui/components/table";
+
+const headers = [
+  { title: "Galpón" },
+  { title: "Huevos C" },
+  { title: "Huevos B" },
+  { title: "Huevos A" },
+  { title: "Huevos AA" },
+  { title: "Huevos AAA" },
+  { title: "Huevos Extra" },
+  { title: "Total Huevos" },
+  { title: "Fecha de recolección" },
+  { title: "Acción" },
+];
+
+const columns = [
+  { column: "sheld" },
+  { column: "categoryC" },
+  { column: "categoryB" },
+  { column: "categoryA" },
+  { column: "categoryAA" },
+  { column: "categoryAAA" },
+  { column: "categoryExtra" },
+  { column: "totalEggs" },
+  { column: "date" },
+  { column: "action" },
+];
+
+const harvest = [
+  {
+    sheld: "Galpón A",
+    categoryC: 120,
+    categoryB: 90,
+    categoryA: 150,
+    categoryAA: 150,
+    categoryAAA: 121,
+    categoryExtra: 180,
+    totalEggs: 811,
+    date: "2025-02-13",
+  },
+  {
+    sheld: "Galpón B",
+    categoryC: 120,
+    categoryB: 90,
+    categoryA: 150,
+    categoryAA: 150,
+    categoryAAA: 121,
+    categoryExtra: 180,
+    totalEggs: 811,
+    date: "2025-02-13",
+  },
+  {
+    sheld: "Galpón C",
+    categoryC: 120,
+    categoryB: 90,
+    categoryA: 150,
+    categoryAA: 150,
+    categoryAAA: 121,
+    categoryExtra: 180,
+    totalEggs: 811,
+    date: "2025-02-13",
+  },
+];
 
 export const HarvestPage = () => {
   return (
     <>
       <Breadcrumb breadCrumb="Recolección" />
-      <h1>HarvestPage</h1>
+      <Table
+        caption="Recolección de Huevos"
+        headers={headers}
+        columns={columns}
+        pageSize={10}
+        data={harvest}
+        totalItems={harvest.length}
+      />
     </>
-  )
-}
+  );
+};
