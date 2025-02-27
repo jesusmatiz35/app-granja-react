d6174af25c6ed480be046a12ff31a3d5cc7b6991
Jesus Matiz <jesusmatiz35@gmail.com>
2025-02-27 15:55:42 -0500

    se ajusta gama de colores

--- src/admin/helpers/menu.js
+++ src/admin/helpers/menu.js
@@ -4,6 +4,6 @@ export const menuItems = [
     { id: 3, name: 'Galpones', path: '/sheds', icon: 'fa-house-chimney-window' },
     { id: 4, name: 'Recolección', path: '/harvest', icon: 'fa-egg' },
     { id: 5, name: 'Costos', path: '/costs', icon: 'fa-money-bill' },
-    { id: 6, name: 'Configuración', path: '/settings', icon: 'fa-gear' },
-    { id: 7, name: 'Reportes', path: '/reports', icon: 'fa-download' },
+    { id: 6, name: 'Reportes', path: '/reports', icon: 'fa-download' },
+    { id: 7, name: 'Configuración', path: '/settings', icon: 'fa-gear' },
 ];
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
   }
 
   return (
-    <nav className="navbar navbar-expand-sm p-2" style={ { backgroundColor: "#191B29", position: 'fixed', width: '100%', marginBottom: '45px', zIndex: '1000' } } >
+    <nav className="navbar navbar-expand-sm p-2" style={ { background: "linear-gradient(135deg, #3f6326 0%,rgb(101, 153, 64) 100%)", position: 'fixed', width: '100%', marginBottom: '45px', zIndex: '1000' } } >
       <Link className="navbar-brand text-warning" to="/dashboard">
         <img src="src/assets/img/granja.png" width="35px" style={ { verticalAlign: 'middle' } } /> <span style={ { verticalAlign: 'middle' } }>Mi Granja App</span>
       </Link>
