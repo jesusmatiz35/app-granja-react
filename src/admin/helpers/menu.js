9a0cf94dd0500e77c80819121953badd9c9c2e96
Jesus Matiz <jesusmatiz35@gmail.com>
2025-03-03 18:06:40 -0500

    se agrega roles al menu

--- src/admin/helpers/menu.js
+++ src/admin/helpers/menu.js
@@ -1,10 +1,10 @@
 export const menuItems = [
-    { name: 'Dashboard', path: '/dashboard', icon: 'fa-gauge' },
-    { name: 'Ventas', path: '/sales', icon: 'fa-basket-shopping' },
-    { name: 'Usuarios', path: '/users', icon: 'fa-users' },
-    { name: 'Galpones', path: '/sheds', icon: 'fa-house-chimney-window' },
-    { name: 'Clientes', path: '/customers', icon: 'fa-user-tie' },
-    { name: 'Reportes', path: '/reports', icon: 'fa-download' },
-    { name: 'Comunidad', path: '/community', icon: 'fa-comments' },
-    { name: 'Configuración', path: '/settings', icon: 'fa-gear' },
+    { name: 'Dashboard', path: '/dashboard', icon: 'fa-gauge', roles: ['Admin', 'User'] },
+    { name: 'Ventas', path: '/sales', icon: 'fa-basket-shopping', roles: ['Admin', 'User']  },
+    { name: 'Usuarios', path: '/users', icon: 'fa-users', roles: ['Admin']  },
+    { name: 'Galpones', path: '/sheds', icon: 'fa-house-chimney-window', roles: ['Admin', 'User']  },
+    { name: 'Clientes', path: '/customers', icon: 'fa-user-tie', roles: ['Admin', 'User']  },
+    { name: 'Reportes', path: '/reports', icon: 'fa-download', roles: ['Admin', 'User']  },
+    { name: 'Comunidad', path: '/community', icon: 'fa-comments', roles: ['Admin', 'User']  },
+    { name: 'Configuración', path: '/settings', icon: 'fa-gear', roles: ['Admin']  },
 ];
