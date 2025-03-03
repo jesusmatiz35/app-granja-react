6163ff3ebee25158cda994db1697e15292e932d4
Jesus Matiz <jesusmatiz35@gmail.com>
2025-03-03 18:53:11 -0500

    se organiza el formulario de registro de granjas

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -40,12 +40,40 @@ export const RegisterPage = () => {
       <section className="container-form forms">
         <div className="form-custom signup">
           <div className="form-content">
-            <header><img src="src/assets/img/granja.png" width="35px" style={ { verticalAlign: 'middle' } } /> <span className="text-warning" style={ { verticalAlign: 'middle' } }>Crear cuenta</span></header>
+            <header><img src="/src/assets/img/granja.png" width="35px" style={ { verticalAlign: 'middle' } } /> <span className="text-warning" style={ { verticalAlign: 'middle' } }>Crear cuenta</span></header>
             <form autoComplete="off">
-              <FormInput placeholder="Correo electronico" errorMessage={ { required: "Campo obligatorio", invalid: "El correo electronico es invalido" } } onValueChange={onInputChange} type="email" name="email" />
-              <FormInput placeholder="Contraseña" min={6} errorMessage={ { required: "Campo obligatorio", invalid: "La contraseña es invalida" } } onValueChange={onInputChange} type="password" name="password" />
-              <FormInput placeholder="Confirmar contraseña" errorMessage={ { required: "Campo obligatorio", invalid: "La confirmación de la contraseña es invalida" } } onValueChange={onInputChange} type="password" name="repassword" />              
-              { password !== repassword && repassword.length >= password.length && <small className='text-danger' style={ { fontSize: '0.9rem', position: 'absolute', display: 'block', marginTop: '-3px', marginLeft: '10px' } }>Las contraseñas no son iguales</small> }
+              <div className="row">
+                <header><small>Datos de la granja</small></header>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="Nombre de la granja" errorMessage={ { required: "Campo obligatorio", invalid: "El campo es invalido" } } onValueChange={onInputChange} type="text" name="farm_name" />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="Dirección" errorMessage={ { required: "Campo obligatorio", invalid: "El campo es invalido" } } onValueChange={onInputChange} type="text" name="address" />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="Telefono" errorMessage={ { required: "Campo obligatorio", invalid: "El campo es invalido" } } onValueChange={onInputChange} type="text" name="phone" />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="País" errorMessage={ { required: "Campo obligatorio", invalid: "El campo es invalido" } } onValueChange={onInputChange} type="text" name="country" />
+                </div>
+                <header className="mt-4"><small>Datos del propietario</small></header>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="Nombre(s)" errorMessage={ { required: "Campo obligatorio", invalid: "El campo es invalido" } } onValueChange={onInputChange} type="text" name="firstname" />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="Apellido(s)" errorMessage={ { required: "Campo obligatorio", invalid: "El campo es invalido" } } onValueChange={onInputChange} type="text" name="lastname" />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="Correo electronico" errorMessage={ { required: "Campo obligatorio", invalid: "El correo electronico es invalido" } } onValueChange={onInputChange} type="email" name="email_propietario" />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="Contraseña" min={6} errorMessage={ { required: "Campo obligatorio", invalid: "La contraseña es invalida" } } onValueChange={onInputChange} type="password" name="password" />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <FormInput placeholder="Confirmar contraseña" errorMessage={ { required: "Campo obligatorio", invalid: "La confirmación de la contraseña es invalida" } } onValueChange={onInputChange} type="password" name="repassword" />              
+                  { password !== repassword && repassword.length >= password.length && <small className='text-danger' style={ { fontSize: '0.9rem', position: 'absolute', display: 'block', marginTop: '-3px', marginLeft: '10px' } }>Las contraseñas no son iguales</small> }
+                </div>
+              </div>
               <div className="field button-field">
                 <FormButton 
                   label="Registrarse" 
